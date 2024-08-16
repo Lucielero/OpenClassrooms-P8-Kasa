ebdbed109919a959852f3e6048b0b4d8a29233f0
Lucie Leroux <lucieleroux93@gmail.com>
2024-08-16 13:46:39 +0200

    mise en place des différents éléments infos des logements

--- src/components/Logement.jsx
+++ src/components/Logement.jsx
@@ -1,14 +1,26 @@
+import React, {useState} from "react";
 import {useParams} from "react-router-dom";
 import logements from "../data/logements.json";
 
-const findLogementID =(id)=>{
-  return logements.find((logement)=>logement.id === id);
-}
+//Images 
+import ArrowRight from "/images/right_arrow.png";
+import ArrowLeft from "/images/left_arrow.png";
+import starActive from "/images/star_active.png";
+import starInactive from "/images/star_inactive.png";
+import ArrowUp from "/images/arrow_up.png";
+import ArrowDown from "/images/arrow_down.png";
+
+const findLogementID =(id)=> logements.find((logement)=>logement.id === id);
 
 const Logement = () => {
   const{id} = useParams();
   const logement = findLogementID(id);
 
+  const [isDescriptionOpen, setIsDescriptionOpen] = useState(false);
+  const [isEquipmentsOpen, setIsEquipmentsOpen] = useState(false);
+
+  const toggleDescription = () => setIsDescriptionOpen(prev => !prev);
+  const toggleEquipments = () => setIsEquipmentsOpen(prev => !prev);
 
 
   if (!logement) {
@@ -27,12 +39,10 @@ const Logement = () => {
           )
         })}  
       </div>
-
-
       {/*Infos*/}
       <div className="info-container">
-
         <div className="heading">
+          {/*Titre et Localisation*/}
           <div className="title-location">
             <h1>{title}</h1>
             <p>{location}</p>
@@ -42,7 +52,7 @@ const Logement = () => {
           <p>{host.name}</p>
           </div>
         </div>
-
+        {/*Note*/}
         <div className="tags-rating">
           <div className="tags">
             {tags.map((tag, index) => (
@@ -52,13 +62,55 @@ const Logement = () => {
             ))}
           </div>
           <div className="rating">
-            
+            {[...Array(5)].map((_, index) => (
+              <img 
+                key={index}
+                src={index < rating ? starActive : starInactive}
+                alt={index < rating ? "Étoile pleine" : "Étoile vide"}
+                className="star"
+              />
+            ))}
           </div>
-
         </div>
-
+        <div className="dropdown-container">
+          {/*Dropdown Description*/}
+          <div className="dropdown_description" onClick={toggleDescription}>
+            <h2>Description</h2>
+            <img 
+              src={isDescriptionOpen ? ArrowUp : ArrowDown}
+              alt={isDescriptionOpen ? "Flèche vers le haut" : "Flèche vers le bas"}
+              className="dropdown_arrow"
+            />
+            <div>
+              {isDescriptionOpen && (
+                <div className="dropdown_content">
+                  <p>{description}</p>
+                </div>
+              )}
+            </div>
+          </div>
+          {/*Dropdown Equipements*/}
+          <div className="dropdown_equipments" onClick={toggleEquipments}>
+            <h2>Équipements</h2>
+            <img 
+              src={isEquipmentsOpen ? ArrowUp : ArrowDown}
+              alt={isEquipmentsOpen ? "Flèche vers le haut" : "Flèche vers le bas"}
+              className="dropdown_arrow"
+            />
+            <div>
+              {isEquipmentsOpen && (
+                <div className="dropdown_content">
+                  <ul>
+                    {equipments.map((equipments, index) => (
+                      <li key={index}>{equipments}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
-
     </div>
   );
 };
